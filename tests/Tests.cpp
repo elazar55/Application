081a7839131ebe092ac9de72b767a03548305308
elazar55 <elazar55@users.noreply.github.com>
2023-11-11 16:01:24 +0000

    test: update Tests.cpp

--- tests/Tests.cpp
+++ tests/Tests.cpp
@@ -1,5 +1,22 @@
 #include "acutest.h"
+#include <SFML/Graphics.hpp>
+
+void KeyboardMapTest()
+{
+    sf::RenderWindow window(sf::VideoMode(800, 600), "KeyboardMapTest");
+    sf::Event        event;
+    TEST_CHECK(window.isOpen());
+
+    while (window.isOpen())
+    {
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed) window.close();
+        }
+    }
+}
 
 TEST_LIST = {
-    {NULL, NULL}
+    {"KeyboardMapTest", KeyboardMapTest},
+    { NULL,             NULL           }
 };
