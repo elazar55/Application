9fef9a8975c0d5ef72cfa99fa981ff92162d93e3
elazar55 <elazar55@users.noreply.github.com>
2023-11-14 18:24:45 +0000

    test: update Tests.cpp

--- tests/Tests.cpp
+++ tests/Tests.cpp
@@ -1,17 +1,26 @@
 #include "acutest.h"
+#include <iostream>
 #include <queue>
 #include <SFML/Graphics.hpp>
-
+/* ========================================================================== */
+/*                              Class Application                             */
+/* ========================================================================== */
 class Application
 {
   public:
-    Application(int         width  = 800,
-                int         height = 600,
-                std::string title  = "Title") :
-        window_(sf::VideoMode(width, height), title)
+    Application() = default;
+    /* ====================================================================== */
+    /*                               InitWindow                               */
+    /* ====================================================================== */
+    void InitWindow(int         width  = 800,
+                    int         height = 600,
+                    std::string title  = "Title")
     {
+        window_.create(sf::VideoMode(width, height), title);
     }
-
+    /* ====================================================================== */
+    /*                                   Run                                  */
+    /* ====================================================================== */
     void Run()
     {
         while (window_.isOpen())
@@ -20,31 +29,45 @@ class Application
             Draw();
         }
     }
-
   private:
+    /* ====================================================================== */
+    /*                               PollEvents                               */
+    /* ====================================================================== */
     void PollEvents()
     {
         sf::Event event;
 
         while (window_.pollEvent(event))
+        {
             if (event.type == sf::Event::Closed) window_.close();
+            if (event.key.code == sf::Keyboard::Escape) window_.close();
+        }
     }
-
+    /* ====================================================================== */
+    /*                                  Draw                                  */
+    /* ====================================================================== */
     void Draw()
     {
         window_.clear();
         window_.display();
     }
-
+    /* ====================================================================== */
+    /*                       Member Variables                                 */
+    /* ====================================================================== */
     sf::RenderWindow window_;
 };
-
+/* ========================================================================== */
+/*                                    Tests                                   */
+/* ========================================================================== */
 void KeyboardMapTest()
 {
     Application app;
+    app.InitWindow();
     app.Run();
 }
-
+/* ========================================================================== */
+/*                                  Test List                                 */
+/* ========================================================================== */
 TEST_LIST = {
     {"KeyboardMapTest", KeyboardMapTest},
     { NULL,             NULL           }
